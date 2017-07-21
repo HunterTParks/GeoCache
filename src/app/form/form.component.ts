72ce23b19da02c6239566d756035342b0d2685cc
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-07-21 09:21:14 -0700

    add functionality /slightly/ to form component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { GeocacheApiService } from 'geocache-api.service.ts';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
+  providers: [ GeocacheApiService ] 
 })
 export class FormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  Cache: any[] = null;
+  constructor(private GeoCache: CeocacheApiService) { }
+  goGetGeocache(){
+    this.GeoCache.getGeocacheCall().subscribe(response => {
+      this.Cache = response.json();
+    });
   }
-
 }
