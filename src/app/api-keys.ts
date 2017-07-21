be829b97a6d029fed3032d83f3b48f35569f998b
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-07-21 08:52:32 -0700

    add apikey to project

---
+++ src/app/api-keys.ts
@@ -0,0 +1,9 @@
+export var geoKey {
+  apiKey: "AIzaSyDnp4SctGaQVo_AtGLyJ7alF0XGoJSTQaM",
+  authDomain: "geocaching-bed47.firebaseapp.com",
+  databaseURL: "https://geocaching-bed47.firebaseio.com",
+  projectId: "geocaching-bed47",
+  storageBucket: "geocaching-bed47.appspot.com",
+  messagingSenderId: "85469034057"
+}
+
